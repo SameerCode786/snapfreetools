135a4ca42041ae395482eefb369b3f5726f30601
SameerCode786 <sameerwithai50@gmail.com>
2026-07-26 00:08:32 +0500

    verel issue fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["pdfjs-dist", "tesseract.js", "browser-image-compression"],
 };
 
 export default nextConfig;
